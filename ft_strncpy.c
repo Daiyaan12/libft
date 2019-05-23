1cbe0dd99fd931f20fc10783f1a923f044ef94bc
Daiyaan Badat <dbadat@c3r8s6.wethinkcode.co.za>
2019-05-23 12:01:54 +0200

    string copy

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,23 +6,24 @@
 /*   By: dbadat <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 11:43:36 by dbadat            #+#    #+#             */
-/*   Updated: 2019/05/22 12:01:36 by dbadat           ###   ########.fr       */
+/*   Updated: 2019/05/23 12:00:35 by dbadat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <libft.h>
 
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	size_t i;
-	i = 0;
 
+	i = 0;
 	while (src[i] != '\0' && src[i] >= 0)
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	return(dst);
+	return (dst);
 }
