23070442c533c699a8d902571593d9af2f1d4ae8
Daiyaan Badat <dbadat@c3r8s6.wethinkcode.co.za>
2019-05-23 11:46:31 +0200

    string copy

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,13 +6,13 @@
 /*   By: dbadat <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 10:39:27 by dbadat            #+#    #+#             */
-/*   Updated: 2019/05/22 11:11:29 by dbadat           ###   ########.fr       */
+/*   Updated: 2019/05/23 11:45:31 by dbadat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char	ft_strcpy(char *dst, const char *src)
+char	*ft_strcpy(char *dst, const char *src)
 {
 	int i;
 
