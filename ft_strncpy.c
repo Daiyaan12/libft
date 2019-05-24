97aad8e5f888adc41a11aae18d4ba365e8c0fbc5
Daiyaan Badat <dbadat@c3r8s6.wethinkcode.co.za>
2019-05-24 11:32:48 +0200

    strncpy

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: dbadat <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 11:43:36 by dbadat            #+#    #+#             */
-/*   Updated: 2019/05/23 12:00:35 by dbadat           ###   ########.fr       */
+/*   Updated: 2019/05/24 11:32:12 by dbadat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strncpy(char *dst, const char *src, size_t len)
 	size_t i;
 
 	i = 0;
-	while (src[i] != '\0' && src[i] >= 0)
+	while (src[i] != '\0' && i <= len)
 	{
 		dst[i] = src[i];
 		i++;
